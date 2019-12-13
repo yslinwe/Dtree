58bf9e50f8c00e71a3ed03f4d1dcad1185c7e3c8
yslinwe <a15775025@163.com>
2019-12-14 00:13:08 +0800

    Initial commit of gittutor reposistory

---
+++ ubuntudownload/download.py
@@ -0,0 +1,187 @@
+import random
+import unittest
+import os
+import shutil
+from bilibiliupload import *
+import ffmpeg
+import subprocess
+import re
+import multiprocessing as mp
+import time
+from bs4 import BeautifulSoup
+import requests
+import yaml
+import signal
+import sys
+import threading
+from upload import upload_test
+b = Bilibili()
+videolist = []
+new_videos = []
+title = ''
+flag_upload = False
+flag_haved_start =True
+#获取文件的大小,结果保留两位小数，单位为MB'''
+def get_FileSize(filePath):
+    fsize = os.path.getsize(filePath)
+    fsize = fsize/float(1024*1024)
+    return round(fsize,2)
+def popen(cmd,roomid,achorname,targeturl,platform):
+    flag_getdanmu = True
+    file_part =''
+    p = subprocess.Popen(cmd, shell =True,stdout=subprocess.PIPE, bufsize=1)
+    for line in iter(p.stdout.readline, b''):
+        outstr = str(line,'utf-8')
+        
+        #match2 = re.findall(r"当前没有在直播",outstr)
+        #print(match)
+        #if len(match)>0:
+        print(achorname,outstr)
+        match1 = re.findall(r"下载停止",outstr)
+        match2 = re.findall(r"liveStatus\s- (.+)\s",outstr)
+        if len(match2)>0:
+            global flag_upload
+            global flag_haved_start
+            if match2[0].strip() == "1":
+                flag_upload = False
+                flag_haved_start = True
+            else: 
+                flag_upload = True
+                if platform=='huya':
+                    description_names=huya_des(targeturl)
+                elif platform == 'douyu':
+                    description_names=douyu_des(targeturl)
+                p.stdout.close()
+                p.wait()   
+                return description_names
+
+        '''     
+                if flag_getdanmu:
+                    cmd1 = 'node ./huyaDanmu-master/huya.js '+roomid+' ./danmu wanzi'
+                    p1 = subprocess.Popen(cmd1)
+                    flag_getdanmu = False
+        if len(match1)>0:
+            if match1[0].strip() == "下载停止" and not flag_getdanmu:
+                p1.kill()
+        '''
+
+def huya_des(target):
+    req = requests.get(url = target)
+    html = req.text
+    bf = BeautifulSoup(html,'lxml')
+    texts = bf.find_all('div',class_="host-title")
+    return texts[0].text
+
+def douyu_des(target):
+    req = requests.get(url = target)
+    html = req.text
+    bf = BeautifulSoup(html,'lxml')
+    texts = bf.find_all('div',class_="Title-headline")
+    return texts[0].text
+
+def cutvideo(achorname,roomid,platform,description_names):
+
+    for f in os.listdir('./download'):
+        if f.endswith('flv') and f[0:len(achorname)] == achorname:
+            name = os.path.basename(f)
+            MatchName = re.search(r"(\D.+)-(.+)\s(.+)\s(.+)\s(.+)-(.+)",name)
+            global title
+            title = achorname+'-'+MatchName.group(4).replace('-','.')+'.'+ MatchName.group(5).split('-')[0]+'-'+description_names
+            break
+    numID = 0
+    path = os.getcwd()
+    for f in os.listdir('./download'):
+        if f.endswith('flv') and f[0:len(achorname)] == achorname:
+            fileofname=os.path.join('download',f)
+            output_file = f
+            newFilePath=os.path.join(path,output_file)
+            shutil.move(fileofname, newFilePath)  # 移动
+
+def PFI(num):
+    if num<10:
+        return '0'+str(num)
+    else: 
+        return str(num)
+def cutlcr():
+    numoftime = 1
+    for file in os.listdir('./danmu'):
+        if file.endswith('LRC'):
+            file_t=os.path.join('danmu',file) 
+            s=os.path.getsize(file_t)
+            if (s/1024)<=1: #单位为k
+                os.remove(file_t)
+
+    lcrfile = [f for f in os.listdir('./danmu') if f.endswith('LRC')]
+    path = './danmu/'+title
+    isExists=os.path.exists(path)
+    if not isExists:
+        os.makedirs(path)
+
+    for f in lcrfile:
+        newFilePath=os.path.join(path,f)
+        filepath = os.path.abspath('./danmu/' + f)
+        shutil.move(filepath, newFilePath)  # 移动
+    
+def job(targeturl,achorname,roomid,platform,description_names,USERNAME,PASSWORD):
+    try:
+        cutvideo(achorname,roomid,platform,description_names)
+        upload_test(achorname,targeturl,title,USERNAME,PASSWORD)
+    except:pass
+
+def huya_message(targeturl):
+    req = requests.get(url = targeturl)
+    html = req.text
+    bf = BeautifulSoup(html,'lxml')
+    achorname = bf.find_all('h3', class_ = 'host-name')[0].text
+    roomid = bf.find_all('span', class_ = 'host-rid')[0].text 
+    return achorname,roomid
+def douyu_message(targeturl):
+    req = requests.get(url = targeturl)
+    html = req.text
+    bf = BeautifulSoup(html,'lxml')
+    achorname = bf.find_all('a', class_ = 'Title-anchorName')[0].text
+    roomid = targeturl.split('/')[len(targeturl.split('/'))-1] 
+    return achorname,roomid
+def processs_task(targeturl,USERNAME,PASSWORD):
+    allfilelist = []
+    allfilelist_two = []
+
+    platform = targeturl.split('.')[1]
+    if platform == 'huya':
+        print(targeturl)
+        achorname,roomid = huya_message(targeturl)
+    elif platform == 'douyu':
+        print(targeturl)
+        achorname,roomid = douyu_message(targeturl)
+
+    while True:
+        cmd = 'java -Dfile.encoding=utf-8 -jar BiliLiveRecorder.jar '+'"'+'debug=false&check=true&fileSize=1024&liver='+platform+'&qn=0&retry=0&id='+roomid+'"'
+        description_names=popen(cmd,roomid,achorname,targeturl,platform)
+        print(description_names)
+        global flag_haved_start
+        for file in os.listdir('./download'):
+            if file.endswith('flv') and file[0:len(achorname)] == achorname:
+                file_t=os.path.join('download',file) 
+                s=os.path.getsize(file_t)
+                if (s/1024/1024)<1:
+                    os.remove(file_t)
+        if flag_upload and flag_haved_start: 
+            print(achorname+'上传文件')
+            p1 = threading.Thread(target=job,args=(targeturl,achorname,roomid,platform,description_names,USERNAME,PASSWORD))
+            p1.start()
+            flag_haved_start = False
+        time.sleep(10)
+
+if __name__ == '__main__':
+
+    with open("config.yaml", "r") as yaml_file:
+        yaml_obj = yaml.load(yaml_file.read(),Loader=yaml.FullLoader)
+    global USERNAME
+    global PASSWORD
+    USERNAME = yaml_obj['USERNAME']
+    PASSWORD = yaml_obj['PASSWORD']
+    targeturl = yaml_obj['link']
+    
+    for target in targeturl:
+        task = mp.Process(target=processs_task,args=(target,USERNAME,PASSWORD))
+        task.start()
---
+++ ubuntudownload/upload.py
@@ -0,0 +1,272 @@
+import json
+import os,shutil
+from selenium import webdriver
+import selenium.common
+import time
+from slider import slider_cracker
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+# 10800 18000 4110
+chromedrive_path = 'chromedriver'
+user_name = ''
+pass_word = ''
+
+class Upload(object):
+    def __init__(self, title):
+        self.title = title
+        # self.date = date
+        # self.url = url
+
+    @property
+    def file_list(self):
+        file_list = []
+        for file_name in os.listdir('.'):
+            if self.title in file_name:
+                file_list.append(file_name)
+        file_list = sorted(file_list)
+        return file_list
+
+    @staticmethod
+    def remove_filelist(file_list):
+        #if not os.path.exists("直播录像"):
+            #os.makedirs("直播录像")                #创建路径
+        for r in file_list:
+            os.remove(r)
+            #shutil.move(r,"直播录像")          #移动文件
+            #print('移动到直播录像-' + r)
+
+    def filter_file(self):
+        file_list = self.file_list
+        if len(file_list) == 0:
+            return False
+        for r in file_list:
+            file_size = os.path.getsize(r) / 1024 / 1024 / 1024
+            if file_size <= 0.001:
+                os.remove(r)
+                print('过滤删除-' + r)
+        file_list = self.file_list
+        if len(file_list) == 0:
+            print('视频过滤后无文件可传')
+            return False
+        for f in file_list:
+            if f.endswith('.part'):
+                os.rename(f, os.path.splitext(f)[0])
+                print('%s存在已更名' % f)
+        return True
+
+    @staticmethod
+    def assemble_videopath(file_list):
+        root = os.getcwd()
+        videopath = ''
+        for i in range(len(file_list)):
+            file = file_list[i]
+            videopath += root + '/' + file + '\n'
+        videopath = videopath.rstrip()
+        return videopath
+
+    @staticmethod
+    def is_element_exist(driver, xpath):
+        s = driver.find_elements_by_xpath(xpath=xpath)
+        if len(s) == 0:
+            print("元素未找到:%s" % xpath)
+            return False
+        elif len(s) == 1:
+            return True
+        else:
+            print("找到%s个元素：%s" % (len(s), xpath))
+            return False
+
+    def upload(self, title_, file_list, link):
+        while True:
+            filename = 'bilibili.cookie'
+            # title_ = self.r_title
+            videopath = self.assemble_videopath(file_list)
+
+            # service_log_path = "{}/chromedriver.log".format('/home')
+            chrome_options = Options()
+            chrome_options.add_argument('--headless')
+            chrome_options.add_argument("--no-sandbox")
+            chrome_options.add_argument('--disable-gpu')
+            chrome_options.add_argument('--disable-dev-shm-usage')
+            driver = webdriver.Chrome(executable_path=chromedrive_path, chrome_options=options)
+            # service_log_path=service_log_path)
+            try:
+
+                driver.get("https://www.bilibili.com")
+                # driver.delete_all_cookies()
+                if os.path.isfile(filename):
+                    with open(filename) as f:
+                        new_cookie = json.load(f)
+
+                    for cookie in new_cookie:
+                        # print(cookie)
+                        driver.add_cookie(cookie)
+
+                driver.get("https://member.bilibili.com/video/upload.html")
+
+                # print(driver.title)
+                self.add_videos(driver, title_, videopath)
+
+                # js = "var q=document.getElementsByClassName('content-header-right')[0].scrollIntoView();"
+                # driver.execute_script(js)
+
+                cookie = driver.get_cookies()
+                with open(filename, "w") as f:
+                    json.dump(cookie, f)
+
+                self.add_information(driver, link, title_)
+
+                driver.find_element_by_xpath('//*[@class="upload-v2-container"]/div[2]/div[3]/div[5]/span[1]').click()
+                # screen_shot = driver.save_screenshot('bin/1.png')
+                # print('截图')
+                time.sleep(3)
+                upload_success = driver.find_element_by_xpath(r'//*[@id="app"]/div/div[3]/h3').text
+                if upload_success == '':
+                    driver.save_screenshot('err.png')
+                    print('稿件提交失败，截图记录')
+                    return
+                else:
+                    print(upload_success)
+                    self.remove_filelist(file_list)
+                    break
+                # print('稿件提交完成！')
+                # print('%s提交完成！' % title_)
+                
+            except selenium.common.exceptions.NoSuchElementException:
+                print('发生错误')
+            # except selenium.common.exceptions.TimeoutException:
+            #     logger.exception('超时')
+            except selenium.common.exceptions.TimeoutException:
+                print('准备更新cookie')
+                # screen_shot = driver.save_screenshot('bin/1.png')
+                WebDriverWait(driver, 10).until(
+                    ec.presence_of_element_located((By.XPATH, r'//*[@id="login-username"]')))
+
+                username = driver.find_element_by_xpath(r'//*[@id="login-username"]')
+                username.send_keys(user_name)
+
+                password = driver.find_element_by_xpath('//*[@id="login-passwd"]')
+                password.send_keys(pass_word)
+                
+                driver.find_element_by_class_name("btn-login").click()
+                # print('第四步')
+                # try:
+                cracker = slider_cracker(driver)
+                cracker.crack()
+                # except:
+                #     logger.exception('出错')
+                time.sleep(5)
+                if driver.title == '投稿 - 哔哩哔哩弹幕视频网 - ( ゜- ゜)つロ 乾杯~ - bilibili':
+                    cookie = driver.get_cookies()
+                    print(cookie)
+                    with open(filename, "w") as f:
+                        json.dump(cookie, f)
+                    print('更新cookie成功')
+                else:
+                    print('更新cookie失败')
+                    time.sleep(10)
+            finally:
+                driver.quit()
+                print('浏览器驱动退出')
+
+    def add_videos(self, driver, title_, videopath):
+        WebDriverWait(driver, 20).until(
+            ec.presence_of_element_located((By.NAME, 'buploader')))
+        upload = driver.find_element_by_name('buploader')
+        # print(driver.title)
+        # print(driver.title)
+        upload.send_keys(videopath)  # send_keys
+        print('开始上传' + title_)
+        time.sleep(2)
+        button = r'//*[@class="new-feature-guide-v2-container"]/div/div/div/div/div[1]'
+        if self.is_element_exist(driver, button):
+            sb = driver.find_element_by_xpath(button)
+            sb.click()
+            sb.click()
+            sb.click()
+            print('点击')
+        while True:
+            # info = driver.find_elements_by_xpath(
+            #     '//*[@id="app"]/div[3]/div[2]/div[2]/div[2]/div/div/div[2]/div[2]')
+            # print(info)
+            info = driver.find_elements_by_class_name(r'item-upload-info')
+            for t in info:
+                # print(t)
+                try:
+                    if t.text != '':
+                        print(t.text)
+                except selenium.common.exceptions.StaleElementReferenceException:
+                    print("selenium.common.exceptions.StaleElementReferenceException")
+
+                # else:
+                #     print('出问题啦')
+            time.sleep(10)
+            # text = driver.find_elements_by_xpath(
+            #     '//*[@id="app"]/div[3]/div[2]/div[2]/div[2]/div/div/div[2]/div[2]/span')
+            text = driver.find_elements_by_xpath(r'//*[@class="item-upload-info"]/span')
+            aggregate = set()
+            for s in text:
+                if s.text != '':
+                    aggregate.add(s.text)
+                    print(s.text)
+            # if text == 'Upload complete' or text == '上传完成':
+            #     break
+
+            if len(aggregate) == 1 and ('Upload complete' in aggregate or '上传完成' in aggregate):
+                break
+        print('上传%s个数%s' % (title_, len(info)))
+
+    @staticmethod
+    def add_information(driver, link, title_):
+        # 点击模板
+        driver.find_element_by_xpath(r'//*[@class="normal-title-wrp"]/div/p').click()
+        driver.find_element_by_class_name(r'template-list-small-item').click()
+        # driver.find_element_by_xpath(
+        #     r'//*[@id="app"]/div[3]/div[2]/div[3]/div[1]/div[1]/div/div[2]/div[1]').click()
+        # 输入转载来源
+        input_o = driver.find_element_by_xpath(
+            '//*[@class="upload-v2-container"]/div[2]/div[3]/div[1]/div[4]/div[3]/div/div/input')
+        input_o.send_keys(link)
+        # 选择分区
+        # driver.find_element_by_xpath(r'//*[@id="item"]/div/div[2]/div[3]/div[2]/div[2]/div[1]/div[2]/div[2]/div[1]/div[3]/div').click()
+        # driver.find_element_by_xpath(r'//*[@id="item"]/div/div[2]/div[3]/div[2]/div[2]/div[1]/div[2]/div[2]/div[1]/div[3]/div[2]/div[6]').click()
+        # 稿件标题
+        title = driver.find_element_by_xpath(
+            '//*[@class="upload-v2-container"]/div[2]/div[3]/div[1]/div[8]/div[2]/div/div/input')
+        title.send_keys(Keys.CONTROL + 'a')
+        title.send_keys(Keys.BACKSPACE)
+        title.send_keys(title_)
+        # js = "var q=document.getElementsByClassName('content-tag-list')[0].scrollIntoView();"
+        # driver.execute_script(js)
+        # time.sleep(3)
+        # 输入相关游戏
+        # driver.save_screenshot('bin/err.png')
+        # print('截图')
+        # text_1 = driver.find_element_by_xpath(
+        #     '//*[@id="item"]/div/div[2]/div[3]/div[2]/div[2]/div[1]/div[5]/div/div/div[1]/div[2]/div/div/input')
+        # text_1.send_keys('星际争霸2')
+        # 简介
+        text_2 = driver.find_element_by_xpath(
+            '//*[@class="upload-v2-container"]/div[2]/div[3]/div[1]/div[12]/div[2]/div/textarea')
+        text_2.send_keys('如有问题，请联系删除')
+
+    def start(self, url,title):
+        print("start")
+        if self.filter_file():
+            print('准备上传' + title)
+            try:
+                self.upload(title, self.file_list, link=url)
+            except selenium.common.exceptions.WebDriverException:
+                print('WebDriverException')
+            # except :
+            #     logger.exception('?')
+def upload_test(achorname,url,title,USERNAME,PASSWORD):
+    global user_name
+    global pass_word
+    user_name = USERNAME
+    pass_word = PASSWORD
+    Upload(achorname).start(url,title)
